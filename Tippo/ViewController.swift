46c2fb0662983acc3c2755e0c014c3acfa2df0a4
Sourav Sachdeva <souravsachdeva13@gmail.com>
2019-12-20 22:05:24 -0800

    Initial Commit

---
+++ Tippo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippo
+//
+//  Created by sourav sachdeva on 20/12/19.
+//  Copyright © 2019 sourav sachdeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
